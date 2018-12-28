45756abfa3c68bd4b6281dea78080988ccd91f8e
Stephen Dowless <stephendowless@HomesnaminsMBP2.fios-router.home>
2018-12-28 12:52:37 -0500

    Initial Commit

---
+++ Pokedex/Controller/PokemonInfoController.swift
@@ -0,0 +1,100 @@
+//
+//  PokemonInfoController.swift
+//  Pokedex
+//
+//  Created by Stephen Dowless on 12/26/18.
+//  Copyright © 2018 Stephan Dowless. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "PokemonStatCell"
+
+class PokemonInfoController: UIViewController {
+    
+    // MARK: - Properties
+    
+    var pokemon: Pokemon?
+    var tableView: UITableView!
+    
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        return iv
+    }()
+    
+    let infoLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    // MARK: - Init
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureViewComponents()
+        fetchPokemonData()
+    }
+    
+    // MARK: - Selectors
+    
+    // MARK: - Helper Functions
+    
+    func configureViewComponents() {
+        view.backgroundColor = .white
+        navigationController?.navigationBar.tintColor = .white
+        
+        view.addSubview(imageView)
+        imageView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: nil, paddingTop: 44, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 100, height: 100)
+        
+        view.addSubview(infoLabel)
+        infoLabel.anchor(top: nil, left: imageView.rightAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 44, paddingLeft: 16, paddingBottom: 0, paddingRight: 4, width: 0, height: 0)
+        infoLabel.centerYAnchor.constraint(equalTo: imageView.centerYAnchor).isActive = true
+    }
+    
+    // MARK: - API
+    
+    func fetchPokemonData() {
+        guard let pokemon = self.pokemon else { return }
+        navigationItem.title = pokemon.name.capitalized
+        imageView.image = pokemon.image
+        
+        Service.shared.fetchPokemonData(forPokemon: pokemon) {
+            self.infoLabel.text = pokemon.description
+            self.configureTableView()
+        }
+    }
+}
+
+extension PokemonInfoController: UITableViewDelegate, UITableViewDataSource {
+    
+    func configureTableView() {
+        tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 50
+        tableView.separatorColor = .mainPink()
+        tableView.isScrollEnabled = false
+        
+        tableView.register(PokemonStatCell.self, forCellReuseIdentifier: reuseIdentifier)
+        
+        view.addSubview(tableView)
+        tableView.anchor(top: imageView.bottomAnchor, left: nil, bottom: nil, right: nil, paddingTop: 32, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: view.frame.width - 64, height: 200)
+        tableView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return PokemonStats.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! PokemonStatCell
+        cell.pokemon = self.pokemon
+        cell.titleLabel.text = PokemonStats(rawValue: indexPath.row)?.description
+        cell.configureCell(withIndex: indexPath.row)
+        return cell
+    }
+}
---
+++ Pokedex/View/InfoView.swift
@@ -0,0 +1,139 @@
+//
+//  InfoView.swift
+//  Pokedex
+//
+//  Created by Stephen Dowless on 12/28/18.
+//  Copyright © 2018 Stephan Dowless. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "PokemonStatCell"
+
+class InfoView: UIView {
+    
+    // MARK: - Properties
+    
+    var pokemon: Pokemon? {
+        didSet {
+            guard let pokemon = self.pokemon else { return }
+            nameLabel.text = pokemon.name.capitalized
+            imageView.image = pokemon.image
+            self.tableView.reloadData()
+            
+            if pokemon.id < 151 {
+                print("Is original pokemon")
+            } else {
+                print("Is not original pokemon")
+            }
+        }
+    }
+    
+    var delegate: PokedexCellDelegate?
+    var tableView: UITableView!
+    
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        return iv
+    }()
+    
+    lazy var nameContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .mainPink()
+        view.addSubview(nameLabel)
+        view.layer.cornerRadius = 5
+        nameLabel.center(inView: view)
+        return view
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        return label
+    }()
+    
+    let infoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .mainPink()
+        button.setTitle("View More Info", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.addTarget(self, action: #selector(handleViewMoreInfo), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureViewComponents()
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleViewMoreInfo() {
+        guard let pokemon = self.pokemon else { return }
+        delegate?.viewMoreInfo(forPokemon: pokemon)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helper Functions
+    
+    func configureViewComponents() {
+        backgroundColor = .white
+        
+        self.layer.masksToBounds = true
+        
+        addSubview(nameContainerView)
+        nameContainerView.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 50)
+        
+        addSubview(imageView)
+        imageView.anchor(top: nameContainerView.bottomAnchor, left: nil, bottom: nil, right: nil, paddingTop: 24, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 100, height: 60)
+        imageView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        
+        configureTableView()
+        
+        addSubview(infoButton)
+        infoButton.anchor(top: nil, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 12, paddingBottom: 12, paddingRight: 12, width: 0, height: 50)
+    }
+}
+
+extension InfoView: UITableViewDelegate, UITableViewDataSource {
+    
+    func configureTableView() {
+        tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 50
+        tableView.separatorColor = .mainPink()
+        tableView.isScrollEnabled = false
+        
+        tableView.register(PokemonStatCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.backgroundColor = .blue
+        
+        addSubview(tableView)
+        tableView.anchor(top: imageView.bottomAnchor, left: nil, bottom: nil, right: nil, paddingTop: 16, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 200, height: 200)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return PokemonStats.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! PokemonStatCell
+        cell.pokemon = self.pokemon
+        cell.titleLabel.text = PokemonStats(rawValue: indexPath.row)?.description
+        cell.configureCell(withIndex: indexPath.row)
+        return cell
+    }
+}
